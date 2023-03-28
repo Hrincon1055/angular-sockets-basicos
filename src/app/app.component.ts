87997c12971b8be03df11c45aa7cd12047fea138
Henry Rincon <henryrinconsantana@gmail.com>
2023-03-27 20:26:43 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'socketsBasicos';
+export class AppComponent implements OnInit {
+  constructor(public websocketService: WebsocketService) {}
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
-
+// CONSTANTES
+import { environment } from '../environments/environment';
+import { FooterComponent } from './components/footer/footer.component';
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FooterComponent],
+  imports: [BrowserModule, SocketIoModule.forRoot(config)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
